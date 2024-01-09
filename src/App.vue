67ff7e9b4c8d900cc3253bcdf468a538c5eb1119
Plaifah <praypar5689@gmail.com>
2024-01-09 12:53:53 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <AboutPage />
+  <ContactPage />
+</template>
+
+<script>
+import AboutPage from './components/HelloWorld.vue'
+import ontactPage from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    AboutPage,
+    ContactPage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
